0b4edaabdad54cc9c8a8c86b11245418296bcc44
Ayush Chirde <71366596+ayush8149@users.noreply.github.com>
2021-08-05 16:18:41 +0530

    Add files via upload

---
+++ 3_AyushChirde.py
@@ -0,0 +1,17 @@
+height = 0
+prev_height = 0
+cnt = 0
+n = input()
+s = raw_input().strip()
+for i in range(len(s)):
+    if (s[i] == 'U'):
+        height += 1
+    elif s[i] == 'D':
+        height -= 1
+    if height == 0 and prev_height < 0:
+        cnt += 1
+    prev_height = height
+if cnt>0:
+    print cnt
+else:
+    print('Please provide valid inputs.')
